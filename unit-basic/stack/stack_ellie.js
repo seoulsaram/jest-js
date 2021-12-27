182e1034626c2ec10f35c6e86cb0bfb9eeb2ee5c
seoulsaram <kaste2233@gmail.com>
2021-12-28 01:51:48 +0900

    stack자료구조 class test : ellie샘 버전

--- unit-basic/stack/stack_ellie.js
+++ unit-basic/stack/stack_ellie.js
@@ -10,21 +10,25 @@ class Stack {
 
   push(item) {
     const node = {item, next: this.head};
+    this.head = node;
+    this._size++;
   }
 
   pop() {
-    if (this.array.length === 0) {
+    if (this.head === null) {
       throw new Error('Stack is empty');
     }
-    return this.array.pop();
+    const node = this.head;
+    this.head = null;
+    this._size--;
+    return node.item;
   }
 
   peek() {
-    if (this.array.length === 0) {
+    if (this.head === null) {
       throw new Error('Stack is empty');
     }
-    //return this.array[this.array.length - 1];
-    return this.array[this.size() - 1];
+    return this.head.item;
   }
 }
 
